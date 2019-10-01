3303667ce81a8affb0adf5e41134f7b6b64cf74f
Narvit <n535786@gmail.com>
2019-10-01 23:25:37 +0600

    UPD_firstmessage

--- web/index.php
+++ web/index.php
@@ -36,7 +36,7 @@ $app->post('/', function() use($app) {
 			$request_params = array(
 				'user_id' => $user_id, 
 				'message' => 'Привет, {$user_name}! Я очень умный бот(нет)', 
-				'acces_token' => getenv('VK_TOKEN'),
+				'access_token' => getenv('VK_TOKEN'),
 				'v' => '5.69'
 			);
 			$get_params = http_build_query($request_params); 
