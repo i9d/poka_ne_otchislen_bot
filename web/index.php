dd4ab35c5348a1123914f7d30c625704a018add0
Narvit <n535786@gmail.com>
2019-10-01 22:26:54 +0600

    UPD7

--- web/index.php
+++ web/index.php
@@ -1,10 +1,6 @@
 <?php
 require('../vendor/autoload.php');
 
-$VK_CONFIGURATION_CODE = '0b9df2d1';
-$VK_SECRET_TOKEN='superkluch9999999pyattrisem';
-$VK_TOKEN='0736a0cdc42087343c259845f58b9a7b12ea38ff65515261bffd0e34bdd6bd509d1380c46013536150b82';
-
 $app = new Silex\Application();
 $app['debug'] = true;
 
@@ -17,20 +13,19 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
 // Our web handlers
 
 $app->get('/', function() use($app) {
- return $VK_CONFIGURATION_CODE;
+ return '0b9df2d1';
 });
 
 $app->post('/', function() use($app) {
 	$data = json_decode(file_get_contents('php://input'));
 	if(!$data) //пустой
-		return 'neok';
-	if($data->secret !== $VK_CONFIGURATION_CODE) && $data->type !== 'confirmation')
+	if($data->secret !== getenv('VK_CONFIRMATION_CODE') && $data->type !== 'confirmation')
 		return 'sovsemneok'; //если токен не совпадает
 	
 	switch($data->type)
 	{
 		case 'confirmation':
-			return $VK_CONFIGURATION_CODE;//VK_CONFIGURATION_CODE:
+			return getenv('VK_CONFIRMATION_CODE:');//VK_CONFIGURATION_CODE:
 			break;
 		
 		case 'message_new':
