6c6d7ddee8101bf93e88ecbf9db3d5fb4c25b43c
Narvit <n535786@gmail.com>
2019-10-01 23:53:13 +0600

    UPD_firstmessage

--- web/index.php
+++ web/index.php
@@ -20,6 +20,8 @@ $app->post('/', function() use($app) {
 	if($data->secret !== getenv('VK_CONFIRMATION_CODE') && $data->type !== 'confirmation')
 		return 'sovsemneok'; //если токен не совпадает
 	
+	$token='0736a0cdc42087343c259845f58b9a7b12ea38ff65515261bffd0e34bdd6bd509d1380c46013536150b82';
+	
 	switch($data->type)
 	{
 		case 'confirmation':
@@ -27,12 +29,11 @@ $app->post('/', function() use($app) {
 			break;
 		
 		case 'message_new':
-			//$token = '0736a0cdc42087343c259845f58b9a7b12ea38ff65515261bffd0e34bdd6bd509d1380c46013536150b82';
 			$user_id = $data->object->user_id; 
-			$user_info = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$user_id}&access_token={getenv('VK_TOKEN');}&v=5.69")); 
+			$user_info = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$user_id}&access_token={$token);}&v=5.69")); 
 			$user_name = $user_info->response[0]->first_name; 
+		
 			
-	
 			$request_params = array(
 				'user_id' => $user_id, 
 				'message' => "Привет, {$user_name}! Я очень умный бот(нет)", 
