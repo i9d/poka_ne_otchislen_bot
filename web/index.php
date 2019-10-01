74f69b2e5cb73565d0d8cadaedb29c67a13e94fd
Narvit <n535786@gmail.com>
2019-10-01 22:42:34 +0600

    UPD_firstmessage

--- web/index.php
+++ web/index.php
@@ -29,7 +29,7 @@ $app->post('/', function() use($app) {
 			break;
 		
 		case 'message_new':
-		/*	$request_params = array)
+			$request_params = array(
 				'user_id' => $data->object->user_id,
 				'message' => 'Привет! Я очень умный бот(нет)',
 				'acces_token' => getenv('VK_TOKEN'),
@@ -37,7 +37,7 @@ $app->post('/', function() use($app) {
 			);
 			
 			file_get_contents('https://api.vk.com/method/messages.send?' . http_build_query($request_params));
-			return 'ok';*/
+			return 'ok';
 		//////
 			break;
 	}
