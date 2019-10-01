188113bceb5483a363552c9d2b8b192d9c209e53
Narvit <n535786@gmail.com>
2019-10-01 23:16:10 +0600

    UPD_firstmessage

--- web/index.php
+++ web/index.php
@@ -30,14 +30,14 @@ $app->post('/', function() use($app) {
 		
 		case 'message_new':
 		
-			//$user_id = $data->object->user_id; 
-			//$user_info = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$user_id}&access_token={getenv('VK_TOKEN')}&v=5.69")); 
-			//$user_name = $user_info->response[0]->first_name; 
+			$user_id = $data->object->user_id; 
+			$user_info = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$user_id}&access_token={getenv('VK_TOKEN')}&v=5.69")); 
+			$user_name = $user_info->response[0]->first_name; 
 			
 	
 			$request_params = array(
 				'user_id' => $user_id, 
-				'message' => 'Привет, ! Я очень умный бот(нет)', //{$user_name}
+				'message' => 'Привет, {$user_name}! Я очень умный бот(нет)', 
 				'acces_token' => getenv('VK_TOKEN'),
 				'v' => '5.69'
 			);
