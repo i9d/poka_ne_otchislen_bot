beb51d754383a6c3cb10ebc2c5b4dae9943923b8
Narvit <n535786@gmail.com>
2019-10-01 21:12:31 +0600

    UPD

--- web/index.php
+++ web/index.php
@@ -1,6 +1,7 @@
 <?php
 
 require('../vendor/autoload.php');
+//getenv('VK_TOKEN');
 
 $app = new Silex\Application();
 $app['debug'] = true;
@@ -10,16 +11,34 @@ $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
 ));
 
-// Register view rendering
-$app->register(new Silex\Provider\TwigServiceProvider(), array(
-    'twig.path' => __DIR__.'/views',
-));
 
 // Our web handlers
 
 $app->get('/', function() use($app) {
-  $app['monolog']->addDebug('logging output.');
-  return $app['twig']->render('index.twig');
+ return "Hello World!";
+});
+
+$app->post('/bot', function() use($app) {
+	$data = json_decode(file_get_contents('php://input'));
+	if(!$data) //пустой
+	if($data->secret !== getenv('VK_SECRET_TOKEN') && $data->type !== 'confirmation')
+		return 'sovsemneok'; //если токен не совпадает
+	
+	switch($data->type)
+	{
+		case 'confirmation':
+			return getenv('VK_SECRET_TOKEN');
+			break;
+		
+		case 'message_new':
+		
+		
+		//////
+			break;
+	}
+	
+	
+ return "oh fignya kakayato";
 });
 
 $app->run();
