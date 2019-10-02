d9793cf3a5c6b839b6b442e89054872c666870a8
Narvit <n535786@gmail.com>
2019-10-02 18:53:57 +0600

    fix

--- web/index.php
+++ web/index.php
@@ -3,28 +3,28 @@ require('../vendor/autoload.php');
 
 $app = new Silex\Application();
 $app['debug'] = true;
-
 // Register the monolog logging service
 $app->register(new Silex\Provider\MonologServiceProvider(), array(
   'monolog.logfile' => 'php://stderr',
 ));
 // Our web handlers
-
 $app->get('/', function() use($app) {
  return 'ну типа работает....';
 });
 
+
+
 $app->post('/', function() use($app) {
 	$data = json_decode(file_get_contents('php://input'));
-	if(!$data) //пустой
 	if($data->secret !== getenv('VK_CONFIRMATION_CODE') && $data->type !== 'confirmation')
-		return 'sovsemneok'; //если токен не совпадает
+		return 'invalid_token';
 	
 	switch($data->type)
 	{
 		case 'confirmation':
-			return getenv('VK_CONFIRMATION_CODE');//VK_CONFIGURATION_CODE:
+			return getenv('VK_CONFIRMATION_CODE');//VK_CONFIGURATION_CODE
 			break;
+		
 		
 		case 'message_new':
 			$token = getenv('VK_TOKEN');
@@ -32,13 +32,24 @@ $app->post('/', function() use($app) {
 			$user_info = json_decode(file_get_contents("https://api.vk.com/method/users.get?user_ids={$user_id}&access_token={$token}&v=5.69")); 
 			$user_name = $user_info->response[0]->first_name; 
 			
-
+			$received_message = $data->object->text;
+			if($received_message == 'Начать')
+			{
+				$send_message = "Привет, {$user_name}! Я супер крутой бот 2999!"; //Внизу появились кнопочки, выбери нужную и нажми на нее.";
+			}
+		//	button_type($received_message);
+			else
+			{
+				$send_message = "{$user_name},я не очень умный бот, поэтому не понимаю, что ты пишешь.";// Пожалуйста, используй кнопки, если их нет, напиши !кнопки";
+			}
+			
 			$request_params = array(
 				'user_id' => $user_id, 
-				'message' => "Привет, {$user_name}! Я очень умный бот(нет)", 
+				'message' => $send_message, 
 				'access_token' => getenv('VK_TOKEN'),
 				'v' => '5.69'
 			);
+
 			$get_params = http_build_query($request_params); 
 			file_get_contents('https://api.vk.com/method/messages.send?'. $get_params); 
 			return('OK');
@@ -48,10 +59,20 @@ $app->post('/', function() use($app) {
 		default:
 			return('OK');
 			break;
-			}
+	}
 	
 	
 	
 });
 
+/*
+function button_type($received_message)
+{
+	switch($received_message)
+	{
+		case '
+	
+	return 
+}
+*/
 $app->run();
