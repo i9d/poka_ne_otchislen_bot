3e3999cc37a3c1504947f64fccce2f0f75c3f99a
Narvit <n535786@gmail.com>
2019-11-11 19:11:06 +0600

    explode_test

--- web/schedule.php
+++ web/schedule.php
@@ -13,7 +13,7 @@ $str = substr($str, -$position);	*/
 $strmass = explode(",", $str);
 $lesson = $strmass[0];
 $teacher = $strmass[1];
-$classroom = $strmass[count($strmass)-1];
+$classroom = count($strmass);
 
 $classroom = $str;
 $sort .= "Предмет: ";
