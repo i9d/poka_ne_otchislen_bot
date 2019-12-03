b43537e4c6906c949a47cd938e786d32545545a5
Narvit <55033607+Narvit@users.noreply.github.com>
2019-12-03 18:55:01 +0600

    Update backup.php

--- web/backup.php
+++ web/backup.php
@@ -1,5 +1,5 @@
 <?php
 $html = file_get_contents('http://fkn.univer.omsk.su/academics/Schedule/schedule3_1.htm');
-$backup = "backup.html"
-file_put_contents($backup, $html);
+//$backup = "backup.html"
+//file_put_contents($backup, $html);
 ?>
