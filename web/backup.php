408e3a712c5136150f8bbe660c2fe88c0ba28137
Narvit <55033607+Narvit@users.noreply.github.com>
2019-12-03 19:00:41 +0600

    Update backup.php

--- web/backup.php
+++ web/backup.php
@@ -1,5 +1,5 @@
 <?php
 $html = file_get_contents('http://fkn.univer.omsk.su/academics/Schedule/schedule3_1.htm');
-$backup = 'vendor/bin/heroku-php-apache2 web/backup.html'
+$backup = 'vendor/bin/heroku-php-apache2 web/backup.html';
 file_put_contents($backup, $html);
 ?>
